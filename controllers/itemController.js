900193e055a715693db6e594466fc3665d69c7da
Agustin Gramajo <agustingramajo@gmail.com>
2021-09-13 20:51:49 -0300

    Developing home view

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,4 +1,24 @@
 var Item = require('../models/item');
+var Category = require('../models/category');
+var Brand = require('../models/brand');
+
+var async = require('async');
+
+exports.index = function(req, res) {
+    async.parallel({
+        item_count: function(callback) {
+            Item.countDocuments({}, callback); // Pass an empty object as match condition to find all documents of this collection
+        },
+        category_count: function(callback) {
+            Category.countDocuments({}, callback);
+        },
+        brand_count: function(callback) {
+            Brand.countDocuments({}, callback);
+        }
+    }, function(err, results) {
+        res.render('index', { title: 'Inventory Home', error: err, data: results });
+    });
+};
 
 // Display list of all Items.
 exports.item_list = function(req, res) {
---
+++ routes/catalog.js
@@ -0,0 +1,90 @@
+var express = require("express");
+var router = express.Router();
+
+// Require controller modules.
+var brand_controller = require("../controllers/brandController");
+var category_controller = require("../controllers/categoryController");
+var item_controller = require("../controllers/itemController");
+
+/// ITEM ROUTES ///
+
+// GET catalog home page.
+router.get("/", item_controller.index);
+
+// GET request for creating a item. NOTE This must come before route that displays item (uses id).
+router.get("/item/create", item_controller.item_create_get);
+
+//POST request for creating item.
+router.post("/item/create", item_controller.item_create_post);
+
+// GET request to delete item.
+router.get("/item/:id/delete", item_controller.item_delete_get);
+
+// POST request to delete item.
+router.post("/item/:id/delete", item_controller.item_delete_post);
+
+// GET request to update item.
+router.get("/item/:id/update", item_controller.item_update_get);
+
+// POST request to update item.
+router.post("/item/:id/update", item_controller.item_update_post);
+
+// GET request for one item.
+router.get("/item/:id", item_controller.item_detail);
+
+// GET request for list of all item.
+router.get("/items", item_controller.item_list);
+
+/// BRAND ROUTES ///
+
+// GET request for creating a brand. NOTE This must come before routes that display brand (uses id).
+router.get("/brand/create", brand_controller.brand_create_get);
+
+// POST request for creating brand.
+router.post("/brand/create", brand_controller.brand_create_post);
+
+// GET request to delete brand.
+router.get("/brand/:id/delete", brand_controller.brand_delete_get);
+
+// POST request to delete brand.
+router.post("/brand/:id/delete", brand_controller.brand_delete_post);
+
+// GET request to update brand.
+router.get("/brand/:id/update", brand_controller.brand_update_get);
+
+// POST request to update brand.
+router.post("/brand/:id/update", brand_controller.brand_update_post);
+
+// GET request for one brand.
+router.get("/brand/:id", brand_controller.brand_detail);
+
+// GET request for list of all brand items.
+router.get("/brands", brand_controller.brand_list);
+
+/// category ROUTES ///
+
+// GET request for creating category. NOTE This must come before route for id (i.e. display category).
+router.get("/category/create", category_controller.category_create_get);
+
+// POST request for creating category.
+router.post("/category/create", category_controller.category_create_post);
+
+// GET request to delete category.
+router.get("/category/:id/delete", category_controller.category_delete_get);
+
+// POST request to delete category.
+router.post("/category/:id/delete", category_controller.category_delete_post);
+
+// GET request to update category.
+router.get("/category/:id/update", category_controller.category_update_get);
+
+// POST request to update category.
+router.post("/category/:id/update", category_controller.category_update_post);
+
+// GET request for one category.
+router.get("/category/:id", category_controller.category_detail);
+
+// GET request for list of all categories.
+router.get("/categories", category_controller.category_list);
+
+module.exports = router;
